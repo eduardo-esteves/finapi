f8cb01194fd78d5cd1c29d2073a39b778bc7fc6d
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-06-13 20:05:32 -0300

    Criado as rotas de testes, com todos os verbos http

--- src/index.js
+++ src/index.js
@@ -9,4 +9,24 @@ app.get('/', (req, resp) => {
   })
 })
 
+app.get('/courses', (req, resp) => {
+  return resp.json(['Curso 1', 'Curso 2', 'Curso 3'])
+})
+
+app.post('/courses', (req, resp) => {
+  return resp.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
+})
+
+app.put('/courses/:id', (req, resp) => {
+  return resp.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4'])
+})
+
+app.patch('/courses/:id', (req, resp) => {
+  return resp.json(['Curso 7', 'Curso 8', 'Curso 3', 'Curso 4'])
+})
+
+app.delete('/courses/:id', (req, resp) => {
+  return resp.json(['Curso 2', 'Curso 3', 'Curso 4'])
+})
+
 app.listen(3000)
