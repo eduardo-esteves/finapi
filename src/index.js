868ec75abefdff01641fb4721b2b6a210566475e
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-06-13 21:29:15 -0300

    Instalado a lib uuid para gerar um id unico na insercao dos dados, novamente declarado o middleware para conseguirmos manipular os dados vindo do body da requisicao

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
+app.use(express.json())
+
+const customers = []
+
+
+app.post('/account', (req, resp) => {
+  const {name, cpf} = req.body
+  const id = uuidv4()
+  customers.push({id, name, cpf, statement: []})
+
+
+  return resp.status(201).send(customers)
+})
+
 
 
 app.listen(3000)
