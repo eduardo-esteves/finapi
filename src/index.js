3a393860c97d8718ae2e17e609d5e257b8fb785f
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-06-16 18:40:30 -0300

    Criado logica para validar e inibir que um cliente seja cadastrado mais de uma vez.

--- src/index.js
+++ src/index.js
@@ -8,12 +8,24 @@ const customers = []
 
 
 app.post('/account', (req, resp) => {
-  const {name, cpf} = req.body
-  const id = uuidv4()
-  customers.push({id, name, cpf, statement: []})
 
+  const {name, cpf} = req.body
+
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+
+  if(customerAlreadyExists) {
+    return resp.status(400).json({error: 'Customer already exists!'})
+  }
+
+  customers.push({
+    id: uuidv4(),
+    name,
+    cpf,
+    statement: []
+  })
 
   return resp.status(201).send(customers)
+
 })
 
 
